37ad3fa7b4f467024ab826ec250d47d7744da7bb
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-05-12 20:39:08 +0200

    print to console

--- serialReader.py
+++ serialReader.py
@@ -1,13 +1,18 @@
+from typing import List
 import serial
 import datetime
 
-def readFewLines():
+def readFewLines() -> List[str]:
     lines = []
     for i in range(5):
         ser_bytes = ser.readline()
         try:
                 decoded_bytes = ser_bytes[:-2].decode("utf-8")
-                lines.append(decoded_bytes)
+                dt = datetime.datetime.now()
+                time = dt.strftime('%Y-%m-%d %H:%M:%S')
+                tempWithTime = time + ': ' + decoded_bytes 
+                lines.append(tempWithTime)
+                print(tempWithTime)
         except:
             continue
     return lines
@@ -15,10 +20,15 @@ def readFewLines():
 def writeToFile(lines):
     with open('temperatures.txt', 'a') as file:
         for l in lines:
-            file.write(str(datetime.datetime.now()) + ' ' + l + '\n')
+            file.write(l + '\n')
 
-ser = serial.Serial(port='COM3', baudrate=19200)
-#ser.flushInput()
-while True:
-    lines = readFewLines()
-    writeToFile(lines)
+if __name__ == '__main__':
+    arduinoPort = ''
+    with open('config.txt') as configFile:
+        arduinoPort = configFile.readline()
+
+    ser = serial.Serial(port=arduinoPort, baudrate=19200)
+
+    while True:
+        lines = readFewLines() # read few lines and save to file once in a while
+        writeToFile(lines)
