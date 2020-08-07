dbe939220a65468c999c1bcd5bc35e917ae5ae7a
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-08-07 13:33:09 +0200

    add session

--- src/Common/Integration/Mongo/Session.py
+++ src/Common/Integration/Mongo/Session.py
@@ -1,8 +1,16 @@
 import pymongo
 
-
 class Session:
     def __init__(self, mongoConnectionString: str):
         self.client = pymongo.MongoClient(mongoConnectionString)
         self.database = self.client["temperatureProject"]
     
+    def add(self, entity):
+        collection = self.__get_collection(entity)
+        collection.insert_one(entity)
+    
+    def get(self, entity):
+        collection = self.__get_collection(entity)
+
+    def __get_collection(self, entity):
+        return self.database(type(entity).__name__ + 's')
