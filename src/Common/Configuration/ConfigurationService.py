d1ad25aa3dd9f85f656c23e4095ea90509c2ce19
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-08-16 21:55:03 +0200

    add flask to requirements

---
+++ src/Common/Configuration/ConfigurationService.py
@@ -0,0 +1,9 @@
+class ConfigurationService:
+    def __init__(self):
+        super().__init__()
+
+    def get_records_frequency(self):
+        pass
+
+    def save_records_frequency(self, frequency):
+        pass
