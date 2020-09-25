2568e951c565f7863539675613cf930c0d65441c
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-25 22:27:17 +0200

    add handling port exception

--- src/TemperatureScanner/Integration/Adruino/SerialHandler.py
+++ src/TemperatureScanner/Integration/Adruino/SerialHandler.py
@@ -1,17 +1,35 @@
 from typing import List
 from serial import Serial
 from time import sleep
+from os import path
 
 class SerialHandler:
-    _serial = None
+    _serial: Serial = None
+
     def __init__(self, arduinoPort: str):
+        self.arduinoPort = arduinoPort
         if SerialHandler._serial is None:
-            SerialHandler._serial = Serial(arduinoPort, baudrate=250000, write_timeout=0.5, timeout=0.8)
-            sleep(3)
+            self._init_serial()
 
     def write(self, stringToWrite):
-        SerialHandler._serial.write(str(stringToWrite).encode())
+        try:
+            SerialHandler._serial.write(str(stringToWrite).encode())
+        except:
+            print('Arduino not available')
+            sleep(2)
+            self._init_serial()
 
     def read(self) -> List[str]:
-        lines = SerialHandler._serial.readlines()
-        return [x.decode() for x in lines]
+        try:
+            lines = SerialHandler._serial.readlines()
+            return [x.decode() for x in lines]
+        except:
+            print('Arduino not available')
+            sleep(2)
+            self._init_serial()
+    
+    def _init_serial(self):
+        if path.exists(self.arduinoPort):
+            SerialHandler._serial = Serial(self.arduinoPort, baudrate=250000, write_timeout=0.5, timeout=0.8)
+            sleep(3)
+        
