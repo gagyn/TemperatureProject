ba818956b17c3d04ac874bc4f25ddfd411bde17d
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-28 18:47:39 +0200

    change arch to default

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ export DOCKER_BUILDKIT=1
 docker build --platform=local -o . git://github.com/docker/buildx
 mkdir -p ~/.docker/cli-plugins
 mv buildx ~/.docker/cli-plugins/docker-buildx
-#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_LOGIN --password-stdin
 docker buildx build --platform linux/arm32/7 -t gagyn/temperature_project:scanner -f ./src/TemperatureScanner/Dockerfile.arm32 ./src --push
 docker buildx build --platform linux/arm32/7 -t gagyn/temperature_project:nginx -f ./src/nginx/Dockerfile.arm32 ./src/nginx --push
