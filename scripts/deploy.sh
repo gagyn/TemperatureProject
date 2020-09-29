4cf4fc7814711b732b82da63ec953a272ea884f5
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-29 21:47:52 +0200

    add builder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
-sudo systemctl restart docker
-
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_LOGIN --password-stdin
 docker buildx build --platform linux/arm32/7 -t gagyn/temperature_project:scanner -f ./src/TemperatureScanner/Dockerfile.arm32 ./src --push
 docker buildx build --platform linux/arm32/7 -t gagyn/temperature_project:nginx -f ./src/nginx/Dockerfile.arm32 ./src/nginx --push
