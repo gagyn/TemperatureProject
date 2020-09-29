d3434da000aa76d5484497a897faef2da68f14ed
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-09-29 21:41:55 +0200

    try another way

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
 sudo systemctl restart docker
-docker --version
-docker build --platform=local -o . git://github.com/docker/buildx
-mkdir -p ~/.docker/cli-plugins
-mv buildx ~/.docker/cli-plugins/docker-buildx
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_LOGIN --password-stdin
 docker buildx build --platform linux/arm32/7 -t gagyn/temperature_project:scanner -f ./src/TemperatureScanner/Dockerfile.arm32 ./src --push
