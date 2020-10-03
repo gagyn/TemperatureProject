aa6a0b7b91f2dbf7789c79fee7076a625e79d7de
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2020-10-03 22:00:54 +0200

    fix bugs

--- src/TemperatureScanner/Services/FrequentRecordsReaderService.py
+++ src/TemperatureScanner/Services/FrequentRecordsReaderService.py
@@ -11,65 +11,35 @@ class FrequentRecordsReaderService:
         self.arduinoService = arduinoService
         self.mongo = mongo
         self.waitingService = waitingService
-        self.stopped = configurationService.get_reading_state() == 'paused'
-        if self.stopped:
-            self.stop_reading()
-        else:
-            self.start_reading()
-        self._toRestart = False
 
     def start_frequent_service(self):
         time.sleep(1)
-        self.waitingService.start(self._read_and_save)
+        shouldStartAsRunning = self.configurationService.get_reading_state() == 'running'
+        if shouldStartAsRunning:
+            self.waitingService.start(self._read_and_save)
             
     def start_reading(self):
-        self.stopped = False
-        self._toRestart = False
+        self.waitingService.start(self._read_and_save)
         self.configurationService.set_reading_state('running')
 
     def stop_reading(self):
-        self.stopped = True
+        self.waitingService.cancel()
         self.configurationService.set_reading_state('paused')
 
     def restart_reading(self):
-        self._toRestart = True
+        self.waitingService.cancel()
+        self.waitingService.start(self._read_and_save)
 
     def make_temperature_entity(self, value: float, recordsCount: int, sensorName: str) -> dict:
         return {'createdAt': datetime.datetime.utcnow(), 'value': value, 'basedOnRecordsCount': recordsCount, 'sensorNameId': sensorName}
 
     def _read_and_save(self):
         try:       
-            temperatureEntity = self._read_temperature()
+            (temperature, basedOnRecordsCount) = self.arduinoService.read_now()
+            temperatureEntity = self.make_temperature_entity(temperature, basedOnRecordsCount, 'out1')
             self._add_to_base(temperatureEntity)
         except Exception as e:
             print(e)
 
-    def _wait_if_stopped(self):
-        while self.stopped:
-            time.sleep(1)
-
-    def _read_temperature(self) -> dict:
-        (temperature, basedOnRecordsCount) = self.arduinoService.read_now()
-        return self.make_temperature_entity(temperature, basedOnRecordsCount, 'out1')
-
     def _add_to_base(self, temperatureEntity: dict):
         self.mongo.db.temperatures.insert_one(temperatureEntity)
-        
-    def _wait_and_handle_restarts(self):
-        lastSecondBeforeStopped = self._wait_till_next_record()
-        while self._toRestart:
-            self._toRestart = False
-            self._wait_till_next_record(lastSecondBeforeStopped)
-
-    def _wait_till_next_record(self, startWith = 0) -> int: # returns the second when was stopped
-        secondsBetween = self.configurationService.get_records_seconds_between()
-        for i in range(startWith, secondsBetween):
-            time.sleep(1)
-            if self.stopped:
-                return i
-            if self._toRestart:
-                newSecondsBetween = self.configurationService.get_records_seconds_between()
-                if i <= newSecondsBetween:
-                    self._toRestart = False
-                return i
-        return secondsBetween
--- src/TemperatureScanner/Services/WaitingService.py
+++ src/TemperatureScanner/Services/WaitingService.py
@@ -1,23 +1,28 @@
 from time import sleep
 from datetime import datetime
 from Common.Configuration.ConfigurationService import ConfigurationService
+import threading
+import time
 
 class WaitingService:
 
     def __init__(self, configurationService: ConfigurationService):
         self.isCancelled = True
         self.configurationService = configurationService
-        self.secondsBetween = self.configurationService.get_records_seconds_between()
-        self._wait() # run in new thread
+        self._update_seconds_between()
+        thread = threading.Thread(target=self._wait, name='waitingService')
+        thread.start()
 
     def start(self, action):
         self.isCancelled = False
+        self._update_seconds_between()
         self.action = action
 
     def cancel(self):
         self.isCancelled = True
+        sleep(0.6)
 
-    def update_seconds_between(self):
+    def _update_seconds_between(self):
         self.secondsBetween = self.configurationService.get_records_seconds_between()
 
     def _wait(self):
@@ -30,7 +35,7 @@ class WaitingService:
                 self.action()
 
     def _how_many_seconds_should_wait(self) -> int:
-        timeInSeconds = (datetime.now() - datetime.now().date()).total_seconds
+        timeInSeconds = int(time.time())
         howLongShouldWait = self.secondsBetween - timeInSeconds % self.secondsBetween
         return howLongShouldWait
 
